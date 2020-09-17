f2b9cda6f6b9236c79cc19187afb1eda19859ab6
unknown <nikitasotnikov0@gmail.com>
2020-09-17 22:40:31 +0300

    сделал, вариант 2.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите длину окружности L: ");
+                double l = double.Parse(Console.ReadLine());
+
+                double r = l / (2 * Math.PI);
+                double s = Math.PI * Math.Pow(r, 2);
+
+                Console.WriteLine($"Радиус круга: {r:0.00}\nПлощадь круга: {s:0.00}");
+                Console.ReadKey();
+            }
+           
+            catch
+            {
+                Console.WriteLine("Ошибка! Неправельные данные!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
